beced7c8e5ae46b2e3ede8587c9aca37c3a36b64
JanAdell <36292051+JanAdell@users.noreply.github.com>
2023-06-10 00:36:58 +0200

    Working on delaying audio loop from chimes

--- Chime_Fodie_S_Final/Assets/Scripts/ChimeScript.cs
+++ Chime_Fodie_S_Final/Assets/Scripts/ChimeScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ChimeScript : MonoBehaviour
 {
     //this script will be used mainly for the chime's SFX control (delay) and showing the player the pickup related UI
+    AudioSource audioSource;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayDelayed(5.0f);
     }
 
     // Update is called once per frame
